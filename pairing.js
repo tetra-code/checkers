663f07875d47d2817cad390004021c17c0323fd4
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-07-19 13:15:54 +0200

    created pairing class and object

---
+++ pairing.js
@@ -0,0 +1,24 @@
+// module for creating multiplayer game pairs
+const pairing = () => {
+    this.player1 = null;
+    this.player2 = null;
+    this.player1Connection = null;
+    this.player2Connection = null;
+};
+
+// if player1 is null than 
+pairing.prototype.addPlayerAndConnection = (player, connection) => {
+    if (this.player2 !== null) {
+        return new Error("Invalid call. This pairing is full");
+    }
+    if (this.player1 === null) {
+        this.player1 = player;
+        this.player1Connection = connection;
+        return "Added player1";
+    }
+    this.player2 = player;
+    this.player2Connection = connection;
+    return "Added player2";
+};
+
+module.exports = pairing;
