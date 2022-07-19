88c3f3fbb5bd70d25c513d7096701c84eb249abe
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-07-19 13:58:42 +0200

    fix pairing class

--- app.js
+++ app.js
@@ -3,22 +3,28 @@ const app = express();
 const http = require('http');
 const port = process.env.PORT || 3000;
 const socketio = require('socket.io');
-// const { server } = require('websocket');
 const pairing = require('./pairing');
 let games = [];
 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
+app.use(express.static(`${__dirname}/public`, {
+    extensions:['html']
+}));
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Splash screen");
     // res.sendFile('splash.html', { root: "./public" });
 });
 
 app.get('/play', (req, res) => {
-    res.send("Let's play checkers")
+    res.send("Game screen");
     // res.sendFile('game.html'), { root: "./public" };
 });
 
+//default route error handling
+app.get('*', (req, res) => {
+    res.status(404).send("Not found");
+});
 
 
 const server = http.createServer(app);
--- pairing.js
+++ pairing.js
@@ -1,24 +1,24 @@
-// // module for creating multiplayer game pairs
-// const pairing = () => {
-//     this.player1 = null;
-//     this.player2 = null;
-//     this.player1Connection = null;
-//     this.player2Connection = null;
-// };
+// module for creating multiplayer game pairs
+const pairing = function() {
+    this.player1 = null;
+    this.player2 = null;
+    this.player1Connection = null;
+    this.player2Connection = null;
+};
 
-// // if player1 is null than 
-// pairing.prototype.addPlayerAndConnection = (player, connection) => {
-//     if (this.player2 !== null) {
-//         return new Error("Invalid call. This pairing is full");
-//     }
-//     if (this.player1 === null) {
-//         this.player1 = player;
-//         this.player1Connection = connection;
-//         return "Added player1";
-//     }
-//     this.player2 = player;
-//     this.player2Connection = connection;
-//     return "Added player2";
-// };
+// if player1 is null then
+pairing.prototype.addPlayerAndConnection = (player, connection) => {
+    if (this.player2 !== null) {
+        return new Error("Invalid call. This pairing is full");
+    }
+    if (this.player1 === null) {
+        this.player1 = player;
+        this.player1Connection = connection;
+        return "Added player1";
+    }
+    this.player2 = player;
+    this.player2Connection = connection;
+    return "Added player2";
+};
 
-// module.exports = pairing;
+module.exports = pairing;
