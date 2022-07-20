7510bb636e3beac994b0cee73f85c186d2fcfd4e
Pil Kyu Cho <comsci.p.cho@gmail.com>
2022-07-20 09:55:21 +0200

    dom element for board boxes

---
+++ public/javascript/gameManager.js
@@ -0,0 +1,39 @@
+const boxB8 = document.getElementById('b8');
+const boxD8 = document.getElementById('a8');
+const boxF8 = document.getElementById('a8');
+const boxH8 = document.getElementById('a8');
+
+const boxA7 = document.getElementById('a7');
+const boxC7 = document.getElementById('c7');
+const boxE7 = document.getElementById('e7');
+const boxG7 = document.getElementById('g7');
+
+const boxB6 = document.getElementById('b6');
+const boxD6 = document.getElementById('d6');
+const boxF6 = document.getElementById('f6');
+const boxH6 = document.getElementById('h6');
+
+const boxA5 = document.getElementById('a5');
+const boxC5 = document.getElementById('c5');
+const boxE5 = document.getElementById('e5');
+const boxG5 = document.getElementById('g5');
+
+const boxB4 = document.getElementById('b4');
+const boxD4 = document.getElementById('d4');
+const boxF4 = document.getElementById('f4');
+const boxH4 = document.getElementById('h4');
+
+const boxA3 = document.getElementById('a3');
+const boxC3 = document.getElementById('c3');
+const boxE3 = document.getElementById('e3');
+const boxG3 = document.getElementById('g3');
+
+const boxB2 = document.getElementById('b2');
+const boxD2 = document.getElementById('d2');
+const boxF2 = document.getElementById('f2');
+const boxH2 = document.getElementById('h2');
+
+const boxA1 = document.getElementById('a1');
+const boxC1 = document.getElementById('c1');
+const boxE1 = document.getElementById('e1');
+const boxG1 = document.getElementById('g1');
